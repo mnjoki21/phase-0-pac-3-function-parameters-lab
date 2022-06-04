71ac0e084a898f6f3fbd14dd8454b8d1810a3cf2
mitchelle <mitchellenjoki7@gmail.com>
2022-06-05 00:24:39 +0300

    Done.

---
+++ index.js
@@ -0,0 +1,22 @@
+function introduction(name) {
+return(`Hi, my name is ${name}.`)
+
+
+}
+introduction()
+
+function introductionWithLanguage(name, language) {
+    return(`Hi, my name is ${name} and I am learning to program in ${language}.`)
+    
+}
+introductionWithLanguage()
+
+
+function introductionWithLanguageOptional(name, language) {
+    return(`Hi, my name is ${name} and I am learning to program in JavaScript.`)
+
+}
+
+function introductionWithLanguageOptional(name, language) {
+return(`Hi, my name is ${name} and I am learning to program in Python.`)
+}
